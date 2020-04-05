a959456ed25b0105278ad68f8e7821bce8e4da2a
Danilo da Silva Pereira <dansilva41@hotmail.com>
2020-04-05 00:26:31 -0300

    Set credentials Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCEjXBdryibTP775WM6ATaEtz8v40w-fKM',
+    authDomain: 'our-wedding-ca716.firebaseapp.com',
+    databaseURL: 'https://our-wedding-ca716.firebaseio.com',
+    projectId: 'our-wedding-ca716',
+    storageBucket: 'our-wedding-ca716.appspot.com',
+    messagingSenderId: '475456889859',
+    appId: '1:475456889859:web:8ca48cb009e5660bcfda36',
+    measurementId: 'G-X9XHXFN7HV'
+  }
+};
